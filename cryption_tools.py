1973e76a0930fb95632675f0d06fa103bf4357c6
Nilusink <65816512+Nilusink@users.noreply.github.com>
2021-06-14 01:03:32 +0200

    Creation

---
+++ cryption_tools.py
@@ -0,0 +1,95 @@
+from contextlib import suppress
+from math import sqrt
+import base64
+
+class extra:
+    def median(string:str, medians:int):
+        parts = list()
+        out = list()
+        for i in range(1, medians+1):
+            if not i==medians:
+                parts.append([int((len(string)-1)/medians*(i-1)), int((len(string)-1)/medians*i)])
+            else:
+                parts.append([int((len(string)-1)/medians*(i-1)), len(string)])
+        for part in parts:
+            out.append(string[::-1][part[0]:part[1]])
+        return ''.join(out[::-1])
+
+class low:
+    def encrypt(string:str) -> str:
+        out = str()
+        for charter in string:
+            part = str(sqrt(ord(charter)-20))
+            out+=str(base64.b85encode(part.encode('utf-16'))).lstrip("b'").rstrip("='")+' '
+        return out
+
+    def decrypt(string:str) -> str:
+        try:
+            out = str()
+            parts = string.split(' ')
+            for part in parts:
+                s = (part+'=').encode()
+                if not s == b'=':
+                    part = float(base64.b85decode(part).decode('utf-16'))
+                    out += chr(int(round(part**2+20, 0)))
+            return out
+        except ValueError:
+            raise ValueError('Not a valid encrypted string!')
+
+class high:
+    def encrypt(string:str) -> str:
+        temp1, temp2 = str(), str()
+        for charter in string:
+            temp1 += low.encrypt((extra.median(charter, 3)+' '))+' '
+        for charter in extra.median(temp1, 13):
+            temp2 += str(ord(charter))+'|1|'
+        temp2 = low.encrypt(temp2)
+        out = extra.median(extra.median(temp2, 152), 72)
+        return extra.median(str(base64.b85encode(out.encode('utf-32'))).lstrip("b'").rstrip("='")[::-1], 327)
+    
+    def decrypt(string:str) -> str:
+        temp1, temp2 = str(), str()
+        string = extra.median(string, 327)[::-1]
+        string = base64.b85decode(string).decode('utf-32')
+        string = extra.median(extra.median(string, 72), 152)
+        string = low.decrypt(string)
+        parts  = string.split('|1|')
+        for part in parts:
+            with suppress(ValueError):
+                temp1 += chr(int(part))
+        temp1 = extra.median(temp1, 13)
+        parts = temp1.split(' ')
+        for part in parts:
+            temp2 += extra.median(low.decrypt(part), 3)
+        return temp2.replace('   ', '|tempspace|').replace(' ', '').replace('|tempspace|', ' ')
+
+if __name__=='__main__':
+    from time import time
+    try:
+        while True:
+            st = input('\n\nSentence? ')
+            start = time()
+            c = extra.median(low.encrypt(extra.median(st, 12)), 6)
+            e = extra.median(low.decrypt(extra.median(c, 6)), 12)
+            end = time()
+            print('Low encryption:')
+            print(c)
+            print(e)
+            print('\nencrypting and decrypting took:', round(end-start, 2))
+            input('Press enter to start high level encryption')
+            print('\nHigh encryption:')
+            start1 = time()
+            c1 = high.encrypt(st)
+            end1 = time()
+            e1 = high.decrypt(c1)
+            end2 = time()
+            print(c1)
+            print(e1)
+            print('\nencrypting took:', round(end1-start1, 2))
+            print('decrypting took:', round(end2-end1, 2))
+            input('\npress enter to continue\n\n')
+
+    except KeyboardInterrupt:
+        print('Closing Cleint...')
+        del (st, start, c, e, end, start1, c1, end1, e1, end2)
+        exit()
---
+++ useful.py
@@ -0,0 +1,85 @@
+from copy import deepcopy
+
+class List:
+    def RemoveAll(lst:list, value): # remove all values from list
+        return list(filter(lambda a: a!=value, lst))
+
+    def FromMatrix(lst:list, index:int):    # make list from matrix (just return all elements of the matrix (2D))
+        for element in lst:
+            yield element[index]
+    
+    def closest(number:float, lst:list):    # check wich element in list is closest to given number
+        cl = 0
+        for element in lst:
+            if abs(element-number)<abs(cl-number):
+                cl = element
+        return cl
+
+class Dict:
+    def Indexes(dictionary:dict):   # returns the indexes of the dictionary
+        return list(dictionary)
+
+    def Values(dictionary:dict):    # returns the values of the dictionary
+        for element in dictionary:
+            yield dictionary[element]
+
+    def inverse(dictionary:dict):   # inverses the dicitonary (so values become indexes and opposite)
+        x = dict()
+        for element in dictionary:
+            x[dictionary[element]] = element
+        return x
+
+class const:
+    def __init__(self, val):
+        self.value = deepcopy(val) # create deepcopy of list (cause python is strange and source lists are dependent on it's cloned lists)
+    
+    def get(self):
+        return deepcopy(self.value)    # return value with an extra deep copy
+    
+    def len(self):
+        return len(self.value)  # return lenght of list (cause why not?)
+
+def arange(*args):  # basically like numpy.arange (range with float as steps) but with roundet output
+    def_args = [0.0, None, 1.0]
+    if len(args)==1:    # if only one argument is given, map it to element 1
+        def_args[1] = args[0]
+    else:
+        for i in range(len(args)):  # else exchange each element with its corresponding new value
+            def_args[i] = args[i]
+
+    x = def_args[0] # set start position of x
+    while x<def_args[1]:
+        yield float(round(x, len(str(def_args[2]).split('.')[1]))) # return x (roundet based on how many decimals the step variable has)
+        x+=def_args[2]  # add step to x
+
+def inverse(value): # inverse a bool or int (or technically also a str) object
+	t = type(value) # type for final convertion
+	val = bool(value)   # bool value so we don't need to handle every single variable type
+	if val==False:  # inverse
+		val=True
+	else:
+		val=False
+	return t(val)   # return converted value
+
+if __name__ == '__main__':
+    from traceback import format_exc
+
+    # mylist = [[1, 2], [1, 2], [1, 2]]
+    # print(list(ListFromMatrix(mylist, 0)))
+    # print(list(ListFromMatrix(mylist, 1)))
+
+    # mydict = {'Name':'John', 'Age':52, 'Gender':'Male'}
+    # print(list(DictIndexes(mydict)))
+
+    #mylist = [12, 15, None, 30, 10, 29, None]
+    #print(List.RemoveAll(mylist, None))
+
+    #print(list(arange(1, 5)))
+
+    while True:
+        try:
+            com = input('>> ')
+            sadjfkljasdkljf = exec(com)
+            
+        except:
+            print(format_exc())
