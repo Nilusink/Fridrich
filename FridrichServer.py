5018a0af2386157df172291ad3e7964901d5a48e
Nilusink <65816512+Nilusink@users.noreply.github.com>
2021-07-11 16:06:00 +0200

    update

--- FridrichServer.py
+++ FridrichServer.py
@@ -569,6 +569,7 @@ if __name__=='__main__':
 
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind((Const.ip, Const.port))
         server.listen()
         debug.debug(Const.ip)
